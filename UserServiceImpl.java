be852b030983ad32bee9aca567ac582e227473d4
gxtkklt <58285026+gxtkklt@users.noreply.github.com>
2019-11-28 15:49:06 +0800

    Add files via upload
    带下载

---
+++ UserServiceImpl.java
@@ -0,0 +1,26 @@
+package com.ts.biz;
+
+
+import com.ts.dao.UserDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ts.biz.UserService;
+
+import java.util.List;
+import java.util.Map;
+
+@Service("userservices")
+public class UserServiceImpl implements UserService {
+
+
+    @Autowired
+    private UserDao userDao;
+
+    @Override
+    public List<Map<String, Object>> queryList() {
+
+        List<Map<String,Object>> list = userDao.queryList();
+
+        return list;
+    }
+}
